10c0f4828a117deb30f11a7a8203660bf4cf7cd9
liolok <liolokit@gmail.com>
2019-07-02 12:36:03 +0800

    fix: listen on tame status more frequently
    Listen on obedience changes, rather than domestication. In this way,
    tamed beefalo won't poop only once after world starts.

--- modmain.lua
+++ modmain.lua
@@ -1,8 +1,8 @@
 local function debug(str) print("[Beefalo Tamed No Poop] " .. str) end
 
 AddPrefabPostInit("beefalo", function(inst)
-    -- On domestication changes, refresh poop spawner status.
-    inst:ListenForEvent("domesticationdelta", function(inst)
+    -- On obedience changes, refresh poop spawner status.
+    inst:ListenForEvent("obediencedelta", function(inst)
         local tamed = inst.components.domesticatable:IsDomesticated()
         local poop_spawner = inst.components.periodicspawner
         if tamed then
