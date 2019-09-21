83c4f32604488745e43e8ccf5af2cf9bbf799e97
liolok <liolokit@gmail.com>
2019-09-21 14:49:38 +0800

    fix: change restart pooping listen event

--- modmain.lua
+++ modmain.lua
@@ -1,12 +1,13 @@
 AddPrefabPostInit("beefalo", function(inst)
-    -- On obedience changes, refresh poop spawner status.
+    -- On obedience changes, check tamed status and determine to stop pooping.
     inst:ListenForEvent("obediencedelta", function(inst)
         local tamed = inst.components.domesticatable:IsDomesticated()
         local poop_spawner = inst.components.periodicspawner
-        if tamed then
-            poop_spawner:Stop()
-        else
-            poop_spawner:Start()
-        end
+        if tamed and poop_spawner then poop_spawner:Stop() end
+    end)
+    -- On beefalo gone feral, restart pooping.
+    inst:ListenForEvent("goneferal", function(inst)
+        local poop_spawner = inst.components.periodicspawner
+        if poop_spawner then poop_spawner:Start() end
     end)
 end)
