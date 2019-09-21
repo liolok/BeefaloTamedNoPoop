b4c79476f23e69c1919dedce85cf18c46feea43f
liolok <liolokit@gmail.com>
2019-09-21 14:46:11 +0800

    chore: remove debug output

--- modmain.lua
+++ modmain.lua
@@ -1,15 +1,11 @@
-local function debug(str) print("[Beefalo Tamed No Poop] " .. str) end
-
 AddPrefabPostInit("beefalo", function(inst)
     -- On obedience changes, refresh poop spawner status.
     inst:ListenForEvent("obediencedelta", function(inst)
         local tamed = inst.components.domesticatable:IsDomesticated()
         local poop_spawner = inst.components.periodicspawner
         if tamed then
-            -- debug("Beefalo is tamed, stop pooping!")
             poop_spawner:Stop()
         else
-            -- debug("Beefalo is feral, start pooping!")
             poop_spawner:Start()
         end
     end)
